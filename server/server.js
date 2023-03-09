3a6c3146338c6715aea0f88d7a549d975f5c39c6
marcel-tapuleasa <sundaymorningman@gmail.com>
2023-03-09 19:36:34 +0200

    Added Allow Credentials

--- server/server.js
+++ server/server.js
@@ -38,7 +38,8 @@ app.use(express.json());
 // app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://hotelstips.netlify.app");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
