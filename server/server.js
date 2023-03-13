fcf62d835bc641cc8ff859e28a83e95dbb0398dc
marcel-tapuleasa <sundaymorningman@gmail.com>
2023-03-13 19:53:56 +0200

    Removed cookie secret

--- server/server.js
+++ server/server.js
@@ -58,7 +58,9 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser(
+  // process.env.COOKIE_SECRET
+  ));
 
 app.use(
   mongoSanitize({
